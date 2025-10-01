8daf274e60db90e56302c4f6276e2940fa1788c3
Dana-Alyahya <s202279460@kfupm.edu.sa>
2025-10-01 12:09:09 +0300

    Dana alyahya

--- 5-2-react-starter/src/App.jsx
+++ 5-2-react-starter/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-
+import StudentCard from './components/StudentCard';
 function App() {
   return (
     <div className="app">
@@ -11,6 +11,9 @@ function App() {
       <main className="dashboard-main">
         <div className="cards-container">
           {/* TODO: Import and render StudentCard components here */}
+            <StudentCard name="Alice Johnson" id="2021001" dept="Computer Science" />
+            <StudentCard name="Bob Smith" id="2021002" dept="Electrical Engineering" />
+            <StudentCard name="Charlie Brown" id="2021003" dept="Mechanical Engineering" />
         </div>
       </main>
     </div>
